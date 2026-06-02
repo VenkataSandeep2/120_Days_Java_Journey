05415ae76fa4955b1a9af611649c48fc0edb85d3
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-02 12:04:04 +0530

    Add files via upload

---
+++ Day_6_Switc_While_DoWhile/src/WhileLoop/EvenNum.java
@@ -0,0 +1,18 @@
+package WhileLoop;
+
+public class EvenNum 
+{
+	public static void main(String[] args) 
+	{
+		int i = 2;
+
+		while(i <= 20){
+
+		    System.out.println(i);
+
+		    i = i + 2;
+		}
+		
+	}
+
+}
