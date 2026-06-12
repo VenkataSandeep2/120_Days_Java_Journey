cee196771517f756d732574a7e63cab794d40252
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-12 18:29:15 +0530

    Update Test1.java

--- Day_9_Methods/Test1.java
+++ Day_9_Methods/Test1.java
@@ -1,4 +1,24 @@
-class java
-  {
-    
-  }
+package practise;
+
+class Test1
+{
+    public void sum()
+    {
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        Test t = new Test();
+
+        t.sum();
+    }
+}
