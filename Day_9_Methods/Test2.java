82632e70f30cb33db6d0112a01972a9c7c8e9037
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-12 18:31:53 +0530

    Create Test2.java

---
+++ Day_9_Methods/Test2.java
@@ -0,0 +1,19 @@
+package practise;
+
+public class Test2 
+{
+	    public void sum(int a, int b)
+	    {
+	        System.out.println("Sum : " + (a + b));
+	    }
+
+	    public static void main(String args[])
+	    {
+	        Test2 c = new Test2();
+
+	        c.sum(10, 20);
+	        c.sum(50, 60);
+	    }
+	}
+
+
