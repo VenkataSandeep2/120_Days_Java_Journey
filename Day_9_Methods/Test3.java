8df471aa1bd7667b7f5548c924b5d34fc2fe177a
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-12 18:34:03 +0530

    Create Test3.java

---
+++ Day_9_Methods/Test3.java
@@ -0,0 +1,20 @@
+package practise;
+
+public class Test3 
+{
+	
+	
+	    public void morn()
+	    {
+	        System.out.println("Good Morning!");
+	    }
+
+	    public static void main(String args[])
+	    {
+	        Test3 g = new Test3();
+
+	        g.morn();
+	    }
+	}
+
+
