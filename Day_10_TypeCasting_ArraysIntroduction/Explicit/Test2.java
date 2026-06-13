b6342ab14f571377d9948b75cd03835670ae8a97
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-13 17:01:47 +0530

    Create Test2.java

---
+++ Day_10_TypeCasting_ArraysIntroduction/Explicit/Test2.java
@@ -0,0 +1,21 @@
+package explicit;
+
+public class Test2 
+{
+	public static void main(String[] args) 
+	{
+		double d = 2569.524;
+		int num = (int)d;
+		System.out.println("Double value -> "+d);
+		System.out.println(("Integer value -> "+num));
+		
+	System.out.println();
+		int num1 = 606;
+		char ch = (char)num1;
+		System.out.println("Intger valus -> "+num1);
+		System.out.println("Character values -> "+ch);
+	
+	
+	}
+
+}
