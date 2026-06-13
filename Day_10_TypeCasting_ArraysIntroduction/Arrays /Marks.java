bd07f400e54ce3083a885aae9dbf2b841e03ac4b
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-13 17:32:02 +0530

    Update Marks.java

--- Day_10_TypeCasting_ArraysIntroduction/Arrays /Marks.java
+++ Day_10_TypeCasting_ArraysIntroduction/Arrays /Marks.java
@@ -23,45 +23,12 @@ public class Marks
 		//System.out.println(":- "+marks[7]);	//java.lang.ArrayIndexOutOfBoundsException
 		
 		System.out.println();
-//	    int[] arr = new int[0];
-//		arr[0] = 1;
-//		System.out.println(arr[0]);		//java.lang.ArrayIndexOutOfBoundsException
-//		
-		System.out.println();
-		System.out.println("=====ARRAY FORMATION========");
-		
-		char[] grades = {'a','b'};
-		System.out.println("Index of 0 is  Grade " + grades[0]);
-		grades[0] = 'c';
-		System.out.println("After Index update of 0 is Grade " + grades[0]);
-		System.out.println("Index of 1 is  Grade " + grades[1]);
-		System.out.println("The Grades are in the Arrya:- > "+ grades[0] +" "+grades[1]);
-		
-		System.out.println();
-		System.out.println("================Boolean=================");
-		boolean[] arr2 = new boolean[1];
-		arr2[0] = true;
-		//arr2[0] = !true;
-		System.out.println(arr2[0]);
-		
-		System.out.println();
-		System.out.println("=============String Array================");
-		String[] str = new String[2];
-		str[0] = "Avs";
-		str[1] = "Sandeep";
-		//str[2] = " hello ";
-		System.out.println("String -> " +str[0]);
-		System.out.println("String -> " + str[1]);
-		System.out.println("String Concanation:-> " + str[0] +" "+ str[1]);
-		//System.out.println(str[2]);		//java.lang.ArrayIndexOutOfBoundsException
-		
-		
 	}
 }
 
-
-/*
-              OUTPUT
+		
+		
+		/*OUTPUT
               
               Telugu :- 90
               Hindi :- 85
@@ -70,20 +37,8 @@ public class Marks
               Science :- 86
               Social :-65
               Physics :- 80
-              
-              
-              =====ARRAY FORMATION========
-              Index of 0 is  Grade a
-              After Index update of 0 is Grade c
-              Index of 1 is  Grade b
-              The Grades are in the Arrya:- > c b
-              
-              ================Boolean=================
-              true
-              
-              =============String Array================
-              String -> Avs
-              String -> Sandeep
-              String Concanation:-> Avs Sandeep
 
-                */
+
+
+
+			*/
