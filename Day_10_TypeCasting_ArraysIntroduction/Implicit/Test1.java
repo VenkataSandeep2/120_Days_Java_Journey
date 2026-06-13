dab59f899ac7aa44460b59e31ce7dea27963e1d9
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-13 16:43:12 +0530

    Update Test1.java

--- Day_10_TypeCasting_ArraysIntroduction/Implicit/Test1.java
+++ Day_10_TypeCasting_ArraysIntroduction/Implicit/Test1.java
@@ -1,4 +1,34 @@
-class Test
-  {
-    
-  }
+package com;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		byte b= 120;
+		short s = b ;
+		int i = s;
+
+		long l = s;
+		float f = l;
+		double d =f;
+		System.out.println(b);
+		System.out.println(i);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		
+		
+	}
+
+}
+
+
+/*
+OUTPUT
+    120
+    120
+    120
+    120
+    120.0
+    120.0
