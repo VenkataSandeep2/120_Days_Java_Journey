48a8f38d2ba4d3270c97bbc159bed5b331ce3584
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-13 17:30:41 +0530

    Add Test class to print subject marks

---
+++ Day_10_TypeCasting_ArraysIntroduction/Arrays /Test.java
@@ -0,0 +1,17 @@
+public class Test
+{
+    public static void main(String args[])
+    {
+        int telugu = 90;
+
+        int maths = 99;
+
+        int english = 80;
+
+        System.out.println(telugu);
+
+        System.out.println(maths);
+
+        System.out.println(english);
+    }
+}
