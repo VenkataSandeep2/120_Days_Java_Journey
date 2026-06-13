072f82019da861005f766be21a77538f967e568c
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-13 16:57:00 +0530

    Update Test1.java

--- Day_10_TypeCasting_ArraysIntroduction/Explicit/Test1.java
+++ Day_10_TypeCasting_ArraysIntroduction/Explicit/Test1.java
@@ -1,4 +1,30 @@
-class Test1
-  {
-    
-  }
+package explicit;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		short s = 127;
+		byte b = (byte) s;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		++b;
+		System.out.println(b);
+		
+	}
+
+}
+
+
+/*
+
+Size is exceeded to loop cycle to go the -values
+
+                          OUTPUT
+              
+                    127
+                    -128
+                    -127
+
+*/
