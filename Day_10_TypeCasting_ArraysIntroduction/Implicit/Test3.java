1465265aa3e825121473d474606f240c277004a4
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-13 16:53:15 +0530

    Create Test3.java

---
+++ Day_10_TypeCasting_ArraysIntroduction/Implicit/Test3.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.util.Scanner;
+
+public class Test3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter only Integers values :-> ");
+		int num = sc.nextInt();
+		double d = num;
+		System.out.println("Interger value is -> "+num);
+		System.out.println("Double Value is -> "+d);
+	}
+
+}
