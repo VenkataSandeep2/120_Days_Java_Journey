f2f69e4e0407618d87d72d61f3a49d8e6c922581
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-23 15:34:06 +0530

    Create Test1.java

---
+++ Day_13_Constructor_&_Variaables/Test1.java
@@ -0,0 +1,37 @@
+package com.constructor;
+
+class Student 
+{
+
+    String name;
+    int rollNo;
+
+    Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+    }
+}
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Sanjay", 101);
+
+        s1.display();
+    }
+}
+
+
+/*
+
+OUTPUT
+
+      Name: Sai
+      Roll No: 101
+*/
