c678c37ed45033b48a565fd2a21e62572e806278
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-23 16:02:09 +0530

    Add Vehicle class with TVSRadeon customer care method

---
+++ Day_13_Constructor_&_Variaables/AcessModifier/Vehicle.java
@@ -0,0 +1,22 @@
+package com.practise;
+
+class TVSRadeon 
+{
+
+    public void customerCare() 
+    {
+        System.out.println("Customer Care: 1800-123-456");
+    }
+}
+
+public class Vehicle
+{
+
+    public static void main(String[] args) 
+    {   	
+
+        TVSRadeon bike = new TVSRadeon();
+
+        bike.customerCare();
+    }
+}
