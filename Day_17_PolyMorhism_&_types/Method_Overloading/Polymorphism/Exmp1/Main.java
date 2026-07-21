b1b58c3ec0b72654e164456cfdebb388f1bd0bde
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-07-21 23:14:00 +0530

    Create Main.java

---
+++ Day_17_PolyMorhism_&_types/Method_Overloading/Polymorphism/Exmp1/Main.java
@@ -0,0 +1,11 @@
+package scom.Polymorphism;
+
+public class Main 
+{
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		cal.sum(10,20);
+		cal.sum(10,20,30);
+	}
+
+}
