5bd06e74d6cec3862897ee7db52db0eac900d7c3
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-07-21 23:13:13 +0530

    \

---
+++ Day_17_PolyMorhism_&_types/Method_Overloading/Polymorphism/Exmp1/Calculator.java
@@ -0,0 +1,16 @@
+package scom.Polymorphism;
+
+public class Calculator 
+{
+	public void sum(int a,int b)
+	{
+		System.out.println("Sum of a,b values");
+		System.out.println(a + b);
+	}
+	public void sum(int a,int b,int c)
+	{
+		System.out.println("Sum of a,b,c values");
+		System.out.println( a + b + c);
+	}
+
+}
