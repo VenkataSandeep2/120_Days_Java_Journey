71e9fa16fc7490d700caf353784f5518314e85ef
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-07-23 22:40:56 +0530

    Create Main.java

---
+++ Day_17_PolyMorhism_&_types/Method_OverLoading/Exp_3/Main.java
@@ -0,0 +1,14 @@
+package scom.Polymorphism_3;
+
+public class Main 
+{
+	public static void main(String[] args) {
+		
+	
+	Payment paym = new Payment();
+	paym.pay(1000);
+	paym.pay(15000.23);
+	paym.pay("5428 8657 9832", 56228.2);
+	}
+
+}
