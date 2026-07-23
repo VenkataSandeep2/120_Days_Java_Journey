8011611a1ffc796eaecb8045be4d8ecdfccc9786
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-07-23 22:58:44 +0530

    Create TvRemote.java

---
+++ Day_17_PolyMorhism_&_types/Method_Overriding/Exmp_1/TvRemote.java
@@ -0,0 +1,13 @@
+package scom.Polymorphism_5;
+
+public class TvRemote extends Remote
+{
+	public void turnOn()
+	{
+		System.out.println("tv on");
+	}
+	public void turnOff()
+	{
+		System.out.println("Tv of");
+	}
+}
