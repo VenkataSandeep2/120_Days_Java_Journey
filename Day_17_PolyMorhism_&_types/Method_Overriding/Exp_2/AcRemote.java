4c45910ae5465158ef3076297f35b52eb7198379
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-07-24 13:14:45 +0530

    Create AcRemote.java

---
+++ Day_17_PolyMorhism_&_types/Method_Overriding/Exp_2/AcRemote.java
@@ -0,0 +1,15 @@
+package scom.Polymorphism_6;
+
+public class AcRemote extends Remote 
+{
+	public void turnOn()
+	{
+		System.out.println("Ac turn On");
+	}
+	static void turnOff() 
+	{
+		System.out.println("Ac remote TurnOff");
+	}
+	
+
+}
