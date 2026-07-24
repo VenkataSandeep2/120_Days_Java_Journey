f742f20b002946e6aba7b1333bcaff71112151b5
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-07-24 13:14:16 +0530

    Create Remote.java

---
+++ Day_17_PolyMorhism_&_types/Method_Overriding/Exp_2/Remote.java
@@ -0,0 +1,14 @@
+package scom.Polymorphism_5;
+
+public class Remote 
+{
+	public void turnOn()
+	{
+		System.out.println("Turn on ");
+	}
+	public void turnOff()
+	{
+		System.out.println("Turn Offfffff");
+	}
+
+}
