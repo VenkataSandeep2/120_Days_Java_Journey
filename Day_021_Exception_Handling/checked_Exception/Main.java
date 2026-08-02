7631f48862c52a24463ca61a4fdf64d4e5a950fa
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-02 22:14:38 +0530

    Create Main.java

---
+++ Day_021_Exception_Handling/checked_Exception/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Program Started");
+
+        try
+        {
+            FileReader file =
+                new FileReader("student.txt");
+
+            System.out.println("File Found");
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File Not Found");
+        }
+
+        System.out.println("Program Completed");
+    }
+}
