219b536a3aef06e630764c56f693ca1e20b44fe6
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-16 16:25:21 +0530

    Create StringBuffer_2.java

---
+++ Day_24_String_StringBuilder_buffer/StringBuffer/StringBuffer_2.java
@@ -0,0 +1,15 @@
+public class StringBuffer_2
+{
+    public static void main(String[] args)
+    {
+        StringBuffer cart = new StringBuffer();
+
+        cart.append("Mobile");
+        cart.append(", ");
+        cart.append("Laptop");
+        cart.append(", ");
+        cart.append("Headphones");
+
+        System.out.println("Cart : " + cart);
+    }
+}
