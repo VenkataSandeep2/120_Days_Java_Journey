756dff3934eb1c5e3a21ef4fbf3fc7733a36ec50
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-16 16:23:58 +0530

    Create Stringbuffer_1.java

---
+++ Day_24_String_StringBuilder_buffer/StringBuffer/Stringbuffer_1.java
@@ -0,0 +1,12 @@
+public class Stringbuffer_1
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb =
+                new StringBuffer("Sai");
+
+        sb.append(" Kumar");
+
+        System.out.println(sb);
+    }
+}
