6a94580e9fd11906974d88ba5a2908b90405b820
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-16 16:26:00 +0530

    Create StringBuffer_3.java

---
+++ Day_24_String_StringBuilder_buffer/StringBuffer/StringBuffer_3.java
@@ -0,0 +1,13 @@
+public class StringBuffer_3
+{
+    public static void main(String[] args)
+    {
+        StringBuffer employee =new StringBuffer();
+
+        employee.append("Name : Sandeep\n");
+        employee.append("Age : 22\n");
+        employee.append("Role : Java Developer");
+
+        System.out.println(employee);
+    }
+}
