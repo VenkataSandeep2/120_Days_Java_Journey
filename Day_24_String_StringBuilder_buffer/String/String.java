7a00cfd542ecccf6a3a9439b65ce9abe9568e378
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-16 16:22:06 +0530

    Create String.java

---
+++ Day_24_String_StringBuilder_buffer/String/String.java
@@ -0,0 +1,10 @@
+class String
+  {
+    public static void main(String args[])
+    {
+      String s1 = "Sandeep";
+      String s2 = "Venkata";
+      System.out.println(s1+s2);
+      
+    }
+  }
