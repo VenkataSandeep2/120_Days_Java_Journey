6da2a77127d0be5ce6c70f93080817238dbc9436
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-22 23:10:35 +0530

    Update teacher name in Main class

--- Day_19_Has-A-RelationShip/OOPS_Interview_Questions/Has_A_Relationship/has_a_relationship_1/Main.java
+++ Day_19_Has-A-RelationShip/OOPS_Interview_Questions/Has_A_Relationship/has_a_relationship_1/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
       
-        Teacher t1 = new Teacher(101, "Ravi");
+        Teacher t1 = new Teacher(101, "Ravi Ran");
 
       
         Department d1 = new Department(10, "CSE", t1);
